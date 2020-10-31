05b7f23a4fcc347f49e63d86fa3856462578751f
phongduybui <buiduyphong921@gmail.com>
2020-10-31 10:59:40 +0700

    Refactor code: Create routes, app/controllers folders

--- src/index.js
+++ src/index.js
@@ -1,33 +1,42 @@
 const path = require('path');
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
-const handlebars  = require('express-handlebars');
+const handlebars = require('express-handlebars');
+const app = express();
 const port = 3000;
+
+const route = require('./routes');
 
 //Config static file (img, css...)
 app.use(express.static(path.join(__dirname, 'public')));
 
-//HTTP logger
-app.use(morgan('combined'));
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+// HTTP logger
+// app.use(morgan('combined'));
 
 //Template engine
 app.engine('hbs', handlebars({
-    extname:'.hbs'
+    extname: '.hbs'
 }));
 app.set('view engine', 'hbs');
 
 //Config views folders
 app.set('views', path.join(__dirname, 'resources/views'));
-console.log(path.join(__dirname, 'resources/views'));
- 
+// console.log(path.join(__dirname, 'resources/views'));
 
-app.get('/', function (req, res) {
-    res.render('home');
-});
+//Routes init
+route(app);
 
-app.get('/news', function (req, res) {
-    res.render('news');
-});
+// app.post('/search', (req, res) => {
+//     console.log(req.body);
+//     res.send('');
+// });
+
 // 127.0.0.1 - localhost
-app.listen(port, () => console.log(`App listening at port http://localhost:${port}`))
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+
